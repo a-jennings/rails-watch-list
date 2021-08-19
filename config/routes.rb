cf5fe77e206a61dcec4cc582a708f2f33fd4f7c1
Drew Jennings <andrew_jennings@ymail.com>
2021-08-19 12:54:33 +0100

    add controller for bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists
+  resources :lists do
+    resources :bookmarks, only: %i[new create destroy]
+  end
 end
